d745a347ec40589f847c77f5fadd51e4082eb972
jini-alt <jini@laftel.net>
2026-07-06 19:29:02 +0900

    Update logout.js

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,7 +1,4 @@
-// /api/auth/logout.js
-// 세션 쿠키 삭제 후 /login으로
-
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Set-Cookie', 'session=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0');
   return res.redirect('/login');
 }
